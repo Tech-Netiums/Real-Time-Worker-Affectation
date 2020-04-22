4595ab510c5d6bc9ed68ee82539ae30aba597cf9
Romain Tui <romain.tui@eleves.enpc.fr>
2020-04-22 15:07:08 +0200

    Version Simon, fichiers séparés, TOPSIS fonctionnel

--- Simulateur.py
+++ Simulateur.py
@@ -5,9 +5,11 @@ Created on Wed Mar 18 22:30:22 2020
 """
 import random 
 import math
-import numpy as np
 from termcolor import colored
 import parameters as param
+from productArrival import product_arrival
+from topsis import topsis
+from objet import Product, Worker, Machine, Liberation, Product_arrival
 
 #Paramètre représentant l'influence de la fatigue
 delta = param.delta
@@ -18,81 +20,18 @@ nu = param.nu
 #Cas d'étude
 cas = param.nb_case
 
-class Product : 
-    def __init__(self, number, path, duration):
-        self.number = number
-        self.path = path 
-        self.duration = duration 
-        self.arrivals = []
-        self.departures = []
-        
-        
-#Création des 4 types de produits
-class Product : 
-    def __init__(self, number, path, duration):
-        self.number = number
-        self.path = path 
-        self.duration = duration  
-        self.arrivals = []
-        self.departures = []
-        
+#On crée les listes d'objet : produits, ouvriers et machines
 Products = []
 for i in range(1,5):
     Products.append(Product(i, param.path[i-1], param.duration[i-1]))
-
-#Création des 4 travailleurs
-class Worker :
-    def __init__(self, number,  Skills): 
-        self.number = number 
-        self.skills = Skills
-        self.fatigue = 0
-        
-    def increase_fatigue(self, new_fatigue_level): 
-        self.fatigue = new_fatigue_level
     
 Workers = []
 for i in range(1,5):
     Workers.append(Worker(i, param.skill_matrix[cas-1][i-1]))
-
-
-class Machine : 
-    def __init__(self, number, penibility):
-        self.number = number
-        self.queue = []
-        self.occupation = 0
-        self.penibility = penibility
-        
-#Création des 8 machines
-class Machine : 
-    def __init__(self, number, penibility):
-        self.number = number
-        self.queue = []
-        self.occupation = 0
-        self.penibility = penibility
-        
+    
 Machines = []
 for i in range(1,9):
     Machines.append(Machine(i, param.penibility[i-1]))
-
-#Création des types d'évènements
-class Event : 
-    def __init__(self, time) :
-        self.time = time 
-        
-class Affectation(Event):
-    pass
-
-class Liberation(Event):
-    def __init__(self, time,  worker_num, machine_num, product): 
-        self.time = time
-        self.worker_num = worker_num
-        self.machine_num = machine_num
-        self.product = product
-
-class Product_arrival(Event): 
-    def __init__(self, time, num_prod):
-        self.time = time
-        self.number = num_prod
 
 
 ### Liste d'évenements  et de travailleurs disponibles
@@ -100,7 +39,6 @@ class Product_arrival(Event):
 Events = []
 
 Waiting_workers = []
-
 
 
 def next_event(Events):
@@ -115,7 +53,7 @@ def next_event(Events):
             machine.queue.append(product)
             product.arrivals.append(next_event.time)
             print( str(next_event.time) + ' : ' + colored('Product ' + str(num_prod), 'green') +  ' arrives at' + colored(' Machine ' + str(num_machine), 'red') )
-            return basic(next_event.time)  #on lance l'affectation d'employés
+            return heuristique(next_event.time)  #on lance l'affectation d'employés
         else : 
             machine.queue.append(product)
             product.arrivals.append(next_event.time)
@@ -141,7 +79,7 @@ def next_event(Events):
         Waiting_workers.append(Workers[next_event.worker_num - 1]) #on ajoute le travailleurs à la file de travailleurs libres
         
         
-        return basic(next_event.time)
+        return heuristique(next_event.time)
   
 
 def basic(time) :
@@ -172,13 +110,8 @@ def basic(time) :
         if not affected : 
             worker_index+=1
 
-
-
-
-### TOPSIS Affectation
-
-
-def topsis(time) : 
+# Affectation selon l'heuristique définie
+def heuristique(time) : 
     worker_index = 0
     copy_waiting_worker = []
     for i in range(len(Waiting_workers)) :
@@ -198,122 +131,42 @@ def topsis(time) :
         #Second cas : une seule machine disponible, on assigne le worker
         elif len(Machines_available) == 1 :
             machine = Machines_available[0]
-            Waiting_workers.pop(worker_index)
-            machine.occupation = 1
-            product = machine.queue.pop(0) #on retire le produit de la file d'attente
-            time_interval = product.duration[machine.number - 1] #on récupère l'intervalle de temps que peut prendre le produit pour passer sur la machine
-            penibility = machine.penibility
-            initial_duration = random.randrange(time_interval[0],time_interval[1]+1 , 1) #prends un entier dans l'intervalle
-            exact_duration = initial_duration * ( 1 + delta * penibility * (math.log(1 + worker.fatigue) ))
-            update_fatigue(worker, exact_duration, penibility)
-            Events.append(Liberation(time + exact_duration, worker.number, machine.number, product)) # on crée le nouvel évènement
-            Events.sort(key=lambda x: x.time) #on trie selon le temps 
-            print( str(time) + ' : ' + 'Worker ' + str(worker.number) + ' is affected at machine ' + str(machine.number))
+            affectation(machine, worker, worker_index, time)
             
         #Troisième cas : au moins deux machines sont disponibles, utilisation de l'heuristique
         else : 
-            # On construit dans un premier temps la matrice A
-            A = np.zeros((len(Machines_available),2))
-            for i in range(len(Machines_available)) :
-                machine = Machines_available[i]
-                
-                #Critère C1 : SPT
-                time_interval = product.duration[machine.number - 1]
-                initial_duration = random.randrange(time_interval[0],time_interval[1]+1 , 1)
-                penibility = machine.penibility
-                exact_duration = initial_duration * ( 1 + delta * penibility * (math.log(1 + worker.fatigue) ))
-                A[i][0] = exact_duration
-                
-                #Critère C2 : LNQ 
-                queue = machine.queue
-                lower_bound_fatigue = worker.fatigue
-                exact_duration = 0
-                for j in range(len(queue)) : 
-                    product_queued = queue[j]
-                    time_interval = product_queued.duration[machine.number - 1]
-                    initial_duration = random.randrange(time_interval[0],time_interval[1]+1 , 1)
-                    exact_duration += initial_duration * ( 1 + delta * penibility * (math.log(1 + lower_bound_fatigue) ))
-                A[i][1] = exact_duration
-                
-            # On normalise cette matrice 
-            R = np.zeros((len(Machines_available),2))
-            for j in range(2) :
-                norm = 0
-                for i in range(len(Machines_available)) :
-                    norm +=  A[i][j]
-                norm = math.sqrt(norm)
-                for i in range(len(Machines_available)) :
-                    R[i][j] = A[i][j]/norm
-                    
-            #On pondère la matrice
-            V = np.zeros((len(Machines_available),2))
-            for j in range(2) :
-                for i in range(len(Machines_available)) :
-                    V[i][j] = nu[j]*R[i][j]
             
-            #Détermination de la pire et de la meilleure alternative
-            min_C = [V[0][0], V[0][1]]
-            max_C = [V[0][0], V[0][1]]
-            for i in range(len(Machines_available)) :
-                for j in range(2) :
-                    if V[i][j] < min_C[j] :
-                        min_C[j] = V[i][j]
-                    elif V[i][j] < max_C[j] : 
-                        max_C[j] = V[i][j]
-                        
-            #best solution
-            IA = [max_C[1], min_C[0]]
-            #worst solution
-            WA = [max_C[0], min_C[1]]
-            
-            #Mesure de la distance euclidienne 
-            IS = []
-            WS = []
-            for j in range(len(Machines_available)) : 
-                ISj = math.sqrt(V[j][0] - IA[0] + V[j][1] - IA[1])
-                WSj = math.sqrt(V[j][0] - WA[0] + V[j][1] - WA[1])
-                IS.append(ISj)
-                WS.append(WSj)
-                
-            #Calcul de la similitude avec les pires conditions
-            S = []
-            for j in range(len(Machines_available)) : 
-                S.append(IS[j]/(IS[j]+WS[j]))
-            
-            #Détermination du ratio max 
-            maxS = S[0]
-            machine_index = 0
-            for j in range(len(Machines_available)) :
-                if S[j] > maxS :
-                    maxS = S[j]
-                    machine_index = j
-            machine = Machines_available[machine_index]
+            """Ici, on peut utiliser n'importe quelle heuristique en remplaçant
+            la fonction topsis par une autre heuristique"""
+            #On appelle topsis
+            machine = Machines_available[topsis(Machines_available, worker)]
             
             #Affectation
-            Waiting_workers.pop(worker_index)
-            machine.occupation = 1
-            product = machine.queue.pop(0) #on retire le produit de la file d'attente
-            time_interval = product.duration[machine.number - 1] #on récupère l'intervalle de temps que peut prendre le produit pour passer sur la machine
-            penibility = machine.penibility
-            initial_duration = random.randrange(time_interval[0],time_interval[1]+1 , 1) #prends un entier dans l'intervalle
-            exact_duration = initial_duration * ( 1 + delta * penibility * (math.log(1 + worker.fatigue) ))
-            update_fatigue(worker, exact_duration, penibility)
-            Events.append(Liberation(time + exact_duration, worker.number, machine.number, product)) # on crée le nouvel évènement
-            Events.sort(key=lambda x: x.time) #on trie selon le temps 
-            print( str(time) + ' : ' + 'Worker ' + str(worker.number) + ' is affected at machine ' + str(machine.number))
+            affectation(machine, worker, worker_index, time)
          
+            
+#Permet d'affecter un worker à une machine
+def affectation(machine, worker, worker_index, time) :
+    Waiting_workers.pop(worker_index)
+    machine.occupation = 1
+    product = machine.queue.pop(0) #on retire le produit de la file d'attente
+    time_interval = product.duration[machine.number - 1] #on récupère l'intervalle de temps que peut prendre le produit pour passer sur la machine
+    penibility = machine.penibility
+    initial_duration = random.randrange(time_interval[0],time_interval[1]+1 , 1) #prends un entier dans l'intervalle
+    exact_duration = initial_duration * ( 1 + delta * penibility * (math.log(1 + worker.fatigue) ))
+    update_fatigue(worker, exact_duration, penibility)
+    Events.append(Liberation(time + exact_duration, worker.number, machine.number, product)) # on crée le nouvel évènement
+    Events.sort(key=lambda x: x.time) #on trie selon le temps
+    print( str(time) + ' : ' + 'Worker ' + str(worker.number) + ' is affected at machine ' + str(machine.number))
 
- ##Créer une fonction pour la fatigue               
-        
+
+#Fonction actualisant le niveau de fatigue après calcul                       
 def update_fatigue(worker,duration, penibility):
     fatigue = worker.fatigue
     new_fatigue_level = fatigue + (1 - fatigue) * (1 - math.exp(-penibility*duration))
     worker.increase_fatigue(new_fatigue_level)
-    
- ## Création d'une borne inférieure sur le niveau de fatigue
-
         
-#worker affectation doit renvoyer un evenment liberation avec le worker et ma machine
+#Calcul du mean flowtime
 def MFT() : 
     MFT = []
     for product in Products :
@@ -322,21 +175,39 @@ def MFT() :
             total += product.departures[i] - product.arrivals[i]
         MFT.append(total)
     return MFT
-  
-def generate_items_arrival():
-    time = 0 
-    
+
+#Fonction permettant de dépiler les événements    
 def run(Events): 
     while len(Events) != 0 :
         next_event(Events)
 
- #Test : 
+#Test : 
 
-
-Waiting_workers = [Workers[i] for i in range(len(Workers))]
+"""Waiting_workers = [Workers[i] for i in range(len(Workers))]
 
 Events.append(Product_arrival(0,1))
 Events.append(Product_arrival(0,2))
+Events.append(Product_arrival(0,3))
+Events.append(Product_arrival(0,4))
+Events.append(Product_arrival(10,1))
 run(Events)
 MFT = MFT()
-print(MFT)
+print(MFT)"""
+
+#Journée de 8h
+
+Waiting_workers = [Workers[i] for i in range(len(Workers))]
+
+def average_day() : 
+    #Génération de la totalité des arrivées de produits
+    for i in range(4) : 
+        product_i = product_arrival[i]
+        for j in range (len(product_i)) : 
+            Events.append(Product_arrival(product_i[j],i+1))
+    Events.sort(key=lambda x: x.time) #on trie selon le temps 
+    
+    #On traite les événements
+    run(Events)
+    Average_MFT = MFT()
+    print(Average_MFT)
+    
